5ccc6e6f5f46461b6b79ddfc0f9e3027d984f81a
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-01-11 11:30:28 +0800

    polybar: Major changes to module colors

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -4,19 +4,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Define the tray position monitor
-TRAY_POS="HDMI-1"  # Change this to the monitor name where you want the tray or use "primary" for the primary monitor
-
 # Launch Polybar instances
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ "$m" = "$(xrandr --query | grep primary | cut -d" " -f1)" ]; then
-      TRAY_POS=$m  # Dynamically assign tray position to the primary monitor
-    fi
-    MONITOR=$m TRAY_POS=$TRAY_POS polybar --reload toph &
-  done
+  MONITOR=DP-1 TRAY_POS=DP-1 polybar --reload toph &
+  sleep 0.05
+  MONITOR=HDMI-1 polybar --reload toph &
 else
-    MONITOR=default TRAY_POS=default polybar --reload toph &
+  echo "Fallback: launching Polybar on default monitor"
+  MONITOR=default TRAY_POS=$TRAY_POS polybar --reload toph &
 fi
 
 echo "Bars launched..."
